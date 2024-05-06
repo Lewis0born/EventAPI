c8aace3a87c5c1c56c620171f9e757d6d145d4c4
Lewis Oborn <lewis.oborn@gmail.com>
2024-05-06 16:27:47 +0930

    Begin writing getEvent() fetch request.

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 
-const map = L.map('map').setView([-34.9287, 138.6007], 13); // Adelaide as center with zoom level 13
+const map = L.map('map').setView([-34.9287, 138.6007], 13); // default ([0, 0], 0) Adelaide as center with zoom level 13
 const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributers';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, {attribution});
 tiles.addTo(map);
 
+const api_url = 'https://www.eventbriteapi.com/v3';
 
+async function getEvent() {
+
+    try {
+        const response = await fetch(api_url);
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+
+
+    } catch(error) {
+        console.error(error);
+    }
+
+}
 
